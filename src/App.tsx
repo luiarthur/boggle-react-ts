1eb0f36e41daee7770d5fd7562050bc05bb96492
Arthur Lui <luiarthur@gmail.com>
2021-02-12 09:32:48 -0800

    added seeder file

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-import {Pair} from './Pair'
-
+// import { Seeder } from './Seeder'
+import { Pair } from './Pair'
+import * as util from './util'
 
 function App() {
+  let x = [1,2,3,4,5]
+  let p = new Pair(3,5)
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Boggle</h1>
       </header>
+        <li>random number: {util.randint(0, 1)}</li>
+        <li>variance: {util.variance(x)}</li>
+        <li>Pair: ({[p.x, p.y].join(',')})</li>
     </div>
   );
 }
---
+++ src/Seeder.tsx
@@ -0,0 +1,12 @@
+/// <reference path="util.ts" />
+
+export {}
+
+// export class Seeder extends (props: {seed: number}) {
+//   return (
+//     <input
+//       type="text"
+//       value={this.state.value}
+//       onChange={this.handleChange} />
+//   )
+// }
